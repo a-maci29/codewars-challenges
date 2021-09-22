d9e2a5f9b64e2e85ed66462c9e87151bbabad6fe
Alexa Macias <Alexa Macias>
2021-09-21 22:06:46 -0400

    evenorodd

---
+++ evenorodd/main.js
@@ -0,0 +1,11 @@
+function even_or_odd(number) {
+  let result;
+  if (number % 2 === 0) {
+    result = 'Even';
+  } else {
+    result = 'Odd';
+    }
+return result
+}
+
+//'else' is the fallback, does not require a condition
