343afd436dfc1cf3550a05623c182bce9b8cdcb5
Alexa Macias <Alexa Macias>
2021-09-24 19:45:56 -0400

    countingsheep

---
+++ countingsheep/main.js
@@ -0,0 +1,6 @@
+function countSheeps(arrayOfSheep) {
+  return (arrayOfSheep.filter( e => e === true).length)
+  
+}
+
+// original https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
