30acb81a7d9c97a7113395640108727dfd778ee7
Alexa Macias <Alexa Macias>
2021-09-27 20:49:57 -0400

    balltype

---
+++ balltype/main.js
@@ -0,0 +1,12 @@
+var Ball = function(ballType) {
+   return this.ballType = ballType || "regular"
+
+};
+
+
+  //create class 'Ball'
+  //argument for "ball type"
+  
+  // console log "regular" when no argument is given
+
+  //original: https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
