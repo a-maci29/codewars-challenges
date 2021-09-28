d3d162cbf737fc2fbe814b24d2d11524257e22eb
Alexa Macias <Alexa Macias>
2021-09-27 20:51:56 -0400

    usdcny

---
+++ usdcny/main.js
@@ -0,0 +1,16 @@
+function usdcny(usd) {
+  return (usd * 6.75).toFixed(2) + ' Chinese Yuan';
+}
+
+/*
+
+function usdcny(usd) {}
+
+
+/* 
+let usdcny = (usd) => {
+    return (usd * 6.75).toFixed(2) + ' Chinese Yuan'
+  }
+*/
+
+//original: https://www.codewars.com/kata/5977618080ef220766000022/train/javascript
