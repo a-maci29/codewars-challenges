3527c74b6bc7ff18debe3e7f8670184e70c03943
Alexa Macias <Alexa Macias>
2021-10-16 13:55:20 -0400

    removestringspaces

---
+++ removestringspaces/main.js
@@ -0,0 +1,17 @@
+/* original: https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+Simple, remove the spaces from the string, then return the resultant string.
+
+*/
+
+function noSpace(x){
+    return x.split(" ").join("");
+    //split string
+    //take out any ' '
+    //return
+  
+  }
+  
+  /*let text2 =  
+   text1.split(" ").join("");  */
+
+//const noSpace = (str) => str.replace(/\s*/g, ''); <- literally what the fuck is this??
