751a1083a2c8b2de65bfad64584143182be6258a
Alexa Macias <Alexa Macias>
2021-10-20 22:50:56 -0400

    invertvalues

---
+++ invertvalues/main.js
@@ -0,0 +1,63 @@
+/* Original: https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/javascript
+
+Description:
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list. */
+
+function invert(array) {
+    return array.map(i => 0 - i);
+ }
+
+ function invert(array) {
+     return array.map(i => 0 - i);
+ }
+
+ function invert(array) {
+     return array.map(i => 0 -i);
+ }
+
+ function invert(array) {
+     return array.map(i => 0 - 1);
+ }
+
+ function invert(array) {
+     return array.map(i => 0 - 1);
+ }
+
+function invert(array) {
+    return array.map(i => 0 - 1);
+}
+
+function invert(array) {
+    return array.map(i => 0 - 1);
+}
+
+ ALT: function invert(array) {
+    return array.map(e=> e*-1);
+ }
+
+ function invert(array) {
+     return array.map(e => e * -1);
+ }
+
+ function invert(array) {
+     return array.map(e => e*-1);
+ }
+
+ function invert(array) {
+     return array.map(e => e * -1);
+ }
+
+ function invert(array) {
+     return array.map(e => e * -1);
+ }
+
+ function invert(array) {
+     return array.map(e => e * -1);
+ }
+ 
+ /* From MDN: The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value. */
