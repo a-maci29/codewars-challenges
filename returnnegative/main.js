7fafa9db99514b0f2b691763b8c4fb01e0227d38
Alexa Macias <Alexa Macias>
2021-10-25 10:00:43 -0400

    return negative

---
+++ returnnegative/main.js
@@ -0,0 +1,61 @@
+/* https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+makeNegative(1); // return -1
+makeNegative(-5); // return -5
+makeNegative(0); // return 0
+makeNegative(0.12); // return -0.12
+Notes
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+
+
+
+function makeNegative(num) {
+ //is it going to be a number, or another data type? any strings, arrays, etc?
+  
+  //if num is negative, return ->
+  return num < 0 ? num : (num * -1)
+  //if number is positive, return * -1
+  
+//only integers, any floats or decimal numbers? does that matter?
+}
+function makeNegative(num) {
+return num < 0 ? num : (num * -1)
+}
+
+function makeNegative() {
+return num < 0 ? num : (num * -1)
+}
+
+function makeNegative(num) {
+    return num < 0 ? num : (num * -1)
+}
+
+function makeNegative(num) {
+    return num < 0 ? num : (num * -1)
+}
+
+function makeNegative(num) {
+    return num < 0 ? num : (num * -1)
+}
+
+function makeNegative(num) {
+    return num < 0 ? num : (num * -1)
+}
+/*
+Examples: if -5, return -5
+if 9, return -9
+decimal numbers are okay. ex .07, return -.07
+not checking for other data types - confirmed only numbers
+
+
+P = parameters
+R = return
+E = examples
+P = Psuedocode
+
+*/
