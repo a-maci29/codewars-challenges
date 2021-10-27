5e77f5f37c9c7b245a560b3779d48cbf853d45f3
Alexa Macias <Alexa Macias>
2021-10-26 22:09:59 -0400

    arrayplusarray

---
+++ arrayplusarray/main.js
@@ -0,0 +1,47 @@
+/* Original: https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript
+
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too. */
+
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+/*concat combines two arrays, but doesn't add the value. the reduce() method is what actually adds the values together.
+the acc = accumulator, the cur = current value, as described here:
+https://dev.to/sanderdebr/mastering-the-javascript-reduce-method-2foj
+*/
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+function arrayPlysArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+///ALT: could be done with a for loop
+
+function sumArrays(array1, array2):
+  result = []
+  // assuming arrays are the same length
+  for i in range (0, array.length);
+    result.push(array1[i] + array2[i]
+  return result
