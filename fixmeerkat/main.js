d72f0c2f5ca19314e0a875625a233e76361cf588
Alexa Macias <Alexa Macias>
2021-11-07 15:14:47 -0500

    meerkat

---
+++ fixmeerkat/main.js
@@ -0,0 +1,77 @@
+/* original: https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+Simples!
+
+*/
+
+function fixTheMeerkat(arr) {
+    for (let i = 0; i < fixTheMeerkat.length; i++) {
+      return arr.reverse(fixTheMeerkat);
+    }
+  }
+  
+  function fixTheMeerkat(arr) {
+    for (let i = 0; i < fixTheMeetkat.length; i++) {
+        return arr.reverse(fixTheMeerkat);
+    }
+
+function fixTheMeerkat(arr) {
+    for (let i = 0; i < fixTheMeerkat.length; i++) {
+        return arr.reverse(fixTheMeerkat);
+    }
+}
+
+function fixTheMeerkat(arr) {
+    for (let i = 0; i < fixTheMeerkat.length; i++) {
+        return arr.reverse(fixTheMeerkat);
+    }
+}
+
+function fixTheMeerkat(arr) {
+    for (let i = 0; i < fixTheMeerkat.length; i++) {
+        return arr.reverse(fixTheMeerkat);
+    }
+}
+
+  //ALT:
+
+  function fixTheMeerkat(arr) {
+    return arr.reverse(fixTheMeerkat);
+   }
+
+   function fixTheMeerkat(arr) {
+       return arr.reverse(fixTheMeerkat);
+   }
+
+   function fixTheMeerkat(arr) {
+       return arr.reverse(fixTheMeerkat);
+   }
+  
+   function fixTheMeerkat(arr) {
+       return arr.reverse(fixTheMeerkat);
+   }
+
+   function fixTheMeerkat(arr) {
+       return arr.revrse(fixTheMeerkat);
+   }
+
+   function fixTheMeerkat(arr) {
+       return arr.reverse(fixTheMeerkat);
+   }
+
+  /*
+  P: input is an array that will have 3 values. there will be multiple arrays passed through the function
+    will it always have 3 values? any special chars or data types other than strings?
+    what if the array is empty?
+    will the array always be 'head, body, tail'?
+  R: re-arrange the array; do not make or copy into a new array
+  E: (fixTheMeerkat(["tail", "body", "head"]), ["head", "body", "tail"]);
+  P: loop through each array that will be passed through fixTheMeerkat function
+      reverse the values in each array, without making a new one or a copy
+      return the result
+  */
