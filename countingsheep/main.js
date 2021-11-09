12fb6179d3d37c3e857afc941d5468496e74346f
Alexa Macias <Alexa Macias>
2021-11-09 16:23:21 -0500

    update

--- countingsheep/main.js
+++ countingsheep/main.js
@@ -1,3 +1,19 @@
+/*Original: https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined */
+
 function countSheeps(arrayOfSheep) {
 
   return (arrayOfSheep.filter( e => e === true).length)
@@ -8,4 +24,76 @@ function countSheeps(arrayOfSheep) {
 //^^^testing the type of value that each item in the array will be
 //if true, then it will return
 
-// original https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+function countSHeeps(arrayOfSheep) {
+  return (arrayOfSheep.filter(e => e === true).length)
+}
+
+function countSheeps(arrayOfSheep) {
+  return (arrayOfSheep.filter(e => e === true).length)
+}
+
+function countSheeps(arrayOfSheep) {
+  return (arrayOfSheep.filter(e => e === true).length)
+}
+
+function countSheeps(arrayOfSheep) {
+  return (arrayOfSheep.filter(e => e === true).length)
+}
+
+
+//ALT:
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  
+  arrayOfSheep.forEach( function (sheep) {
+    if (sheep)
+      count++;
+  });
+  
+  return count;
+}
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  arrayOfSheep.forEach(function(sheep) {
+    if (sheep)
+    count++
+  });
+  return count;
+}
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  arrayOfSheep.forEach(function(sheep) {
+    if (sheep)
+    count++
+  });
+  return count;
+}
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  arrayOfSheep.forEach(function(sheep) {
+    if (sheep)
+    count++
+  });
+  return count;
+}
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  arrayOfSheep.forEach(function(sheep) {
+    if (sheep)
+    count++
+  });
+  return count;
+}
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  arrayOfSheep.forEach(function(sheep) {
+    if (sheep)
+    count++
+  });
+  return count;
+}
