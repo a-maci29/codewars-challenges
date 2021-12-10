61ba5149651b5258ad3d7591fe12df946094ce10
Alexa Macias <Alexa Macias>
2021-12-10 12:33:48 -0500

    practice

---
+++ stringToArray.js
@@ -0,0 +1,47 @@
+// Original kata: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+// Write a function to split a string and convert it into an array of words. For example:
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+function stringToArray(string){
+    return string.split(" ")
+    // code code code
+  
+  }
+
+function stringToArray(string) {
+    return string.split(' ')
+}
+
+function stringToArray(string){
+    return string.split(" ")
+    
+}
+//ALT:
+
+const stringToArray = string => string.split(' ');
+
+//stringToArray -> makes the function
+//string -> the parameter
+// => arrow function makes the return implicit in 'string.split(' ');'
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
+
+const stringToArray = string => string.split(' ');
