66fee8bf1b5a6f00fea20b74044378bd297c1d94
Alexa Macias <alexamacias192@gmail.com>
2021-12-30 16:16:11 -0500

    add to array

---
+++ addToArray.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+
+// add the value "codewars" to the already defined websites array
+websites.push("codewars")
+
+// Alt:
+websites[0]="codewars";
+
