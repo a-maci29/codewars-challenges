1fb238c50bba38e9d6fdaf133100c68fe592b20c
Alexa Macias <alexamacias192@gmail.com>
2022-01-13 21:56:05 -0500

    moveEM

---
+++ moveExclamationMarks.js
@@ -0,0 +1,18 @@
+https://www.codewars.com/kata/57fafd0ed80daac48800019f/train/javascript
+
+Move all exclamation marks to the end of the sentence
+
+Examples
+remove("Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!!"
+remove("Hi! Hi! Hi!") === "Hi Hi Hi!!!"
+remove("Hi! !Hi Hi!") === "Hi Hi Hi!!!"
+remove("Hi! Hi!! Hi!") === "Hi Hi Hi!!!!"
+
+
+function remove(s) {
+  return s.replace(/!/g, '') + s.replace(/[^!]/g, '');
+}
+
+//Alt
+remove=s=>s.split('').filter(x=>x!='!').concat(s.split('').filter(x=>x=='!')).join('')
