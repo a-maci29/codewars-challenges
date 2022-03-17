0243a3aa01f472a57ce5fbde68b26db47901eb9a
Alexa Macias <alexamacias192@gmail.com>
2022-03-17 00:01:16 -0400

    add note

--- longestStr.js
+++ longestStr.js
@@ -16,6 +16,8 @@ const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
 const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
 const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
 
+//spread operator creates an array
+
 
 //alt:
 function longest(s1, s2) {
