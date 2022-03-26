8b031ed1b9cb7a2383c9c1da7f7244d3ddc7a66f
Alexa Macias <alexamacias192@gmail.com>
2022-03-26 15:27:32 -0400

    practice

---
+++ reverseddWords.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+function reverseWords(str) {
+  let words = (str.split(" "))
+  let reversed = words.map(e => e.split("").reverse().join(""))
+  
+  return reversed.join(" ").toString()
+}
+
+//alt
+function reverseWords(str) {
+  return str.split(' ').map( str => str.split('').reverse().join('') ).join(' ');
+}
--- twoOldest.js
+++ twoOldest.js
@@ -1,12 +1,12 @@
-https://www.codewars.com/kata/511f11d355fe575d2c000001/train/javascript
+// https://www.codewars.com/kata/511f11d355fe575d2c000001/train/javascript
 
-The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
 
-The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
 
-For example:
+// For example:
 
-twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
 
 function twoOldestAges(ages){
   //sort() the numbers
@@ -23,4 +23,15 @@ function twoOldestAges(ages){
   let oldestToYoungest = ages.sort((a, b) => a-b)
   let sorted =  oldestToYoungest.slice(-2)
   return sorted
+}
+
+//alt:
+function twoOldestAges(ages){
+    let s = ages.sort((a,b) => b-a);
+    return [s[1],s[0]];
+    }
+
+function twoOldestAges(ages){
+    let s = ages.sort((a, b) => b - a);
+    return [s[1], s[0]];
 }
