a598f9b57aaeccc7ceecd4d698d4df4aa2ba9052
Alexa Macias <alexamacias192@gmail.com>
2022-03-31 00:36:00 -0400

    practice

---
+++ sortNumbers.js
@@ -0,0 +1,39 @@
+// https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums){
+  if(nums === null) {
+    return [];
+  }
+  for(let i = 0; i < nums.length; i++) {
+    if( nums[i] > nums[i + 1]) {
+      let newNum = nums[i]
+      nums[i] = nums[i + 1];
+      nums[i + 1] = newNum
+      solution(nums);
+    }
+  }
+  return nums;
+}
+
+//alt
+
+function solution(nums){
+  return nums ? nums.sort((a,b)=>a-b) : []
+}
+
+//alt
+function solution(nums){
+  if(nums === null || nums === []){
+  return []
+  }
+return nums.sort((a, b) => {
+    return a - b
+})
+}
