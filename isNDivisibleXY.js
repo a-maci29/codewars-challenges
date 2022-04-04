581c75c47f92dd7301e5b086c4c2f16fea031ead
Alexa Macias <alexamacias192@gmail.com>
2022-04-03 22:58:21 -0400

    practice

---
+++ isNDivisibleXY.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y === 0) ? true : false;
+}
+
+function isDivisible(n, x, y) {
+  if(n % x === 0 && n % y === 0){
+    return true
+  }else{
+    return false
+  }
+
+}
